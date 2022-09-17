046185b26e019580d298382bb28d147fb47551e5
kwak9898 <kwak9898@github.com>
2022-09-17 17:21:16 +0900

    특정 게시물 삭제 로직 구현

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './board.model';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -18,7 +18,12 @@ export class BoardsController {
   }
 
   @Get(':id')
-  getBoardByid(@Param('id') id: string): Board {
+  getBoardById(@Param('id') id: string): Board {
     return this.boardsService.getBoardById(id);
+  }
+
+  @Delete(':id')
+  deleteBoard(@Param('id') id: string): void {
+    this.boardsService.deleteBoard(id);
   }
 }
