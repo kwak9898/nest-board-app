fb257d62027a351b0bfc6e030369dfa187e7bdf0
kwak9898 <kwak9898@github.com>
2022-09-17 15:51:22 +0900

    git 충돌 해결

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -3,13 +3,9 @@ import { Board } from './board.model';
 
 @Injectable()
 export class BoardsService {
-<<<<<<< HEAD
-  private boards: B
-=======
   private boards: Board[] = [];
 
   getAllBoards(): Board[] {
     return this.boards;
   }
->>>>>>> 45fd3caa9c854e9d1c8f81a2aed6035c2acec901
 }
