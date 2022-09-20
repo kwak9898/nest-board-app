2c6ebeb5e301681bb922ab75f9c9b6cad18b4622
kwaktaemin <kwak9898@github.com>
2022-09-20 18:46:07 +0900

    getBoardById 함수 선언

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -36,17 +36,6 @@ export class BoardsService {
   //   this.boards.push(board);
   //   return board;
   // }
-  //
-  // getBoardById(id: string): Board {
-  //   const found = this.boards.find((board) => board.id === id);
-  //
-  //   if (!found) {
-  //     throw new NotFoundException('게시물을 찾을 수 없습니다.');
-  //   }
-  //
-  //   return found;
-  // }
-  //
   // deleteBoard(id: string): void {
   //   const found = this.getBoardById(id);
   //   this.boards = this.boards.filter((board) => board.id === found.id);
