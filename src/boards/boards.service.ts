743debb910fa14533bd160c31e1ef33afd0d84c5
kwaktaemin <kwak9898@github.com>
2022-09-20 18:48:00 +0900

    주석 제거

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -19,31 +19,4 @@ export class BoardsService {
 
     return found;
   }
-
-  // getAllBoards(): Board[] {
-  //   return this.boards;
-  // }
-  //
-  // createBoard(createBoardDto: CreateBoardDto) {
-  //   const { title, description } = createBoardDto;
-  //   const board: Board = {
-  //     id: uuid(),
-  //     title,
-  //     description,
-  //     status: BoardStatus.PUBLIC,
-  //   };
-  //
-  //   this.boards.push(board);
-  //   return board;
-  // }
-  // deleteBoard(id: string): void {
-  //   const found = this.getBoardById(id);
-  //   this.boards = this.boards.filter((board) => board.id === found.id);
-  // }
-  //
-  // updateBoardStatus(id: string, status: BoardStatus): Board {
-  //   const board = this.getBoardById(id);
-  //   board.status = status;
-  //   return board;
-  // }
 }
