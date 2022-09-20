2698dc9cca4df14157703f9e6dba139f2f6207b9
kwaktaemin <kwak9898@github.com>
2022-09-20 16:11:59 +0900

    커스텀 파이프를 이용한 유효성 체크

---
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,22 @@
+import { BadRequestException, PipeTransform } from '@nestjs/common';
+import { BoardStatus } from '../board.model';
+
+export class BoardStatusValidationPipe implements PipeTransform {
+  readonly StatusOptions = [BoardStatus.PRIVATE, BoardStatus.PUBLIC];
+
+  transform(value: any): any {
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException('올바르지 않은 상태값 입니다.');
+    }
+
+    return value;
+  }
+
+  private isStatusValid(status: any) {
+    const index = this.StatusOptions.indexOf(status);
+
+    return index !== -1;
+  }
+}
