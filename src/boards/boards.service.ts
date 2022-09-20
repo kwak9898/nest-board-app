566482026c2554c4901543127c7bb48052c091fa
kwaktaemin <kwak9898@github.com>
2022-09-20 18:44:46 +0900

    getBoardById 함수 선언

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,7 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { BoardRepository } from './board.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Board } from './board.entity';
 
 @Injectable()
 export class BoardsService {
+  constructor(
+    @InjectRepository(BoardRepository)
+    private boardRepository: BoardRepository,
+  ) {}
+
+  async getBoardById(id: number): Promise<Board> {
+    const found = await this.boardRepository.findOne({ where: { id } });
+
+    if (!found) {
+      throw new NotFoundException('존재하는 게시물이 없습니다.');
+    }
+
+    return found;
+  }
+
   // getAllBoards(): Board[] {
   //   return this.boards;
   // }
