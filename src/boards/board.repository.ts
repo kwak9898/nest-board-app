5dda979384a765d275e2185ce4f1fa666298c9f6
kwaktaemin <kwak9898@github.com>
2022-09-21 14:30:40 +0900

    repository 적용

--- src/boards/board.repository.ts
+++ src/boards/board.repository.ts
@@ -1,5 +1,35 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { Board } from './board.entity';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardStatus } from './board-status.enum';
 
-@EntityRepository(Board)
-export class BoardRepository extends Repository<Board> {}
+@Injectable()
+export class BoardRepository extends Repository<Board> {
+  constructor(private readonly dataSource: DataSource) {
+    super(Board, dataSource.createEntityManager());
+  }
+
+  async getBoardById(id: number): Promise<Board> {
+    const found = await this.findOne({ where: { id } });
+
+    if (!found) {
+      throw new NotFoundException('존재하는 게시물이 없습니다.');
+    }
+
+    return found;
+  }
+
+  async createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+    const { title, description } = createBoardDto;
+
+    const board = this.create({
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    });
+
+    await this.save(board);
+    return board;
+  }
+}
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,38 +1,23 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Board } from './board.entity';
 import { CreateBoardDto } from './dto/create-board.dto';
-import { BoardStatus } from './board-status.enum';
-import { Repository } from 'typeorm';
+import { BoardRepository } from './board.repository';
 
 @Injectable()
 export class BoardsService {
   constructor(
-    @InjectRepository(Board)
-    private boardRepository: Repository<Board>,
+    @InjectRepository(BoardRepository)
+    private boardRepository: BoardRepository,
   ) {}
 
   // 특정 게시물 조회
-  async getBoardById(id: number): Promise<Board> {
-    const found = await this.boardRepository.findOne({ where: { id } });
-
-    if (!found) {
-      throw new NotFoundException('존재하는 게시물이 없습니다.');
-    }
-
-    return found;
+  getBoardById(id: number): Promise<Board> {
+    return this.boardRepository.getBoardById(id);
   }
 
-  async createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
-    const { title, description } = createBoardDto;
-
-    const board = this.boardRepository.create({
-      title,
-      description,
-      status: BoardStatus.PUBLIC,
-    });
-
-    await this.boardRepository.save(board);
-    return board;
+  // 게시물 생성
+  createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+    return this.boardRepository.createBoard(createBoardDto);
   }
 }
