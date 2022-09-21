cfa6d05d0a30cb0148675684d53e4e1039375861
kwaktaemin <kwak9898@github.com>
2022-09-21 20:27:40 +0900

    User & Board Join

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,4 +1,11 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Board } from '../boards/board.entity';
 
 @Entity()
 export class User extends BaseEntity {
@@ -10,4 +17,11 @@ export class User extends BaseEntity {
 
   @Column()
   password: string;
+
+  @OneToMany((type) => Board, (board) => board.user, { eager: true })
+  boards: Board[];
+
+  // async validatePassword(password: string): Promise<boolean> {
+  //   const isValid = await bcrypt.compare;
+  // }
 }
