0ca86a385dd302f39eb38581f3022cd31305c4d1
kwaktaemin <kwak9898@github.com>
2022-09-21 18:34:03 +0900

    비밀번호 암호화 적용

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -6,6 +6,7 @@ import {
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
@@ -16,7 +17,10 @@ export class UserRepository extends Repository<User> {
   // 유저 생성
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<User> {
     const { username, password } = authCredentialsDto;
-    const user = await this.create({ username, password });
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = await this.create({ username, password: hashedPassword });
 
     try {
       return await this.save(user);
