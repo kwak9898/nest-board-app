f28417f8c1e94f0d8021f4f2abaa8a5aa31380c9
kwaktaemin <kwak9898@github.com>
2022-09-21 16:19:55 +0900

    회원가입 API

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
   constructor(private readonly dataSource: DataSource) {
     super(User, dataSource.createEntityManager());
+  }
+
+  // 유저 생성
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<User> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.create({ username, password });
+
+    return await this.save(user);
   }
 }
