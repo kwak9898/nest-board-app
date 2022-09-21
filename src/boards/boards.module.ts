2382c2ebdd333106204bebec83e0068e39e52f69
kwaktaemin <kwak9898@github.com>
2022-09-21 20:03:09 +0900

    유저 인증 게시물 사용

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -4,9 +4,10 @@ import { BoardsService } from './boards.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardRepository } from './board.repository';
 import { Board } from './board.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Board])],
+  imports: [TypeOrmModule.forFeature([Board]), AuthModule],
   controllers: [BoardsController],
   providers: [BoardsService, BoardRepository],
 })
